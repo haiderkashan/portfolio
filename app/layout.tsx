8d5b9ec262ca5a17469b00136f693a819fd399d1
Kashan Haider <kashanhaiderinreallife@gmail.com>
2026-09-01 12:07:58 +0500

    perf: micro-optimize font loading variables on root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="dns-prefetch" href="https://cdn.sanity.io" />
       </head>
-      <body>
+      <body className={fontVariables}>
         <LazyMotion features={domAnimation}>
           {children}
         </LazyMotion>
